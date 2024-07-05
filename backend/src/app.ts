6bd6f4d739311206b1791f16fc448909e74a0c8f
DvornikovAS <DvornikovAS@admlr.lipetsk.ru>
2024-07-05 14:44:55 +0300

    fix: cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,4 @@
+/* eslint-disable comma-dangle */
 /* eslint-disable quotes */
 import "dotenv/config";
 
@@ -17,10 +18,10 @@ mongoose.connect(DB_ADDRESS);
 // Только для локальных тестов. Не используйте это в продакшене
 app.use(
   cors({
-    origin: "https://dvornikovas.mesto.nomorepartiesco.ru",
+    origin: "*",
   })
 );
-app.options("*", cors());
+// app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
